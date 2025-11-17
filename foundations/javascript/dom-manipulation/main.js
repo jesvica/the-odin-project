4fd09fb6845d806a272e5df82883513216a76217
Jessica Williams <jvictoriaw@hotmail.co.uk>
2025-11-17 20:43:56 +0000

    Complete first task - adding elements to container using JavaScript and DOM methods

---
+++ foundations/javascript/dom-manipulation/main.js
@@ -0,0 +1,7 @@
+const container = document.querySelector('#container');
+
+const para = document.createElement('p');
+para.textContent = "Hey I'm red!";
+para.style.color = "red";
+
+container.appendChild(para)
