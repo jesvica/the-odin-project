9b311e87347cd43869c313f873a63af801f78b69
Jessica Williams <jvictoriaw@hotmail.co.uk>
2026-02-19 21:23:00 +0000

    Add new files to redo the lesson and move previous files into separate folder

---
+++ foundations/javascript/dom-manipulation/old/main.js
@@ -0,0 +1,41 @@
+const container = document.querySelector('#container');
+
+// Create a paragraph inside the container
+const para = document.createElement('p');
+para.textContent = "Hey I'm red!";
+para.style.color = "red";
+container.appendChild(para)
+
+// Create a h3 inside the container
+const heading = document.createElement('h3');
+heading.textContent = "I'm a blue h3!"
+heading.style.color = "blue";
+container.appendChild(heading);
+
+// Create a new div inside the container
+const innerContainer = document.createElement('div');
+innerContainer.style.border = "5px solid black";
+innerContainer.style.backgroundColor = "pink";
+
+// Create an h1 inside the inner container
+const mainHeader = document.createElement('h1');
+mainHeader.textContent = "I'm in a div"
+innerContainer.appendChild(mainHeader);
+
+// Create a paragraph inside the inner container
+const innerPara = document.createElement('p');
+innerPara.textContent = "ME TOO!";
+innerContainer.appendChild(innerPara);
+
+// Add the second div to the container
+container.appendChild(innerContainer);
+
+
+// btn.addEventListener("click", function (e) {
+//   console.log(e.target);
+// });
+
+btn.addEventListener("click", function (e) {
+  e.target.style.background = "blue";
+});
+
