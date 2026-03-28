ac668d936e907d7f2cdf386c506bde37b79acf6e
Jessica Williams <jvictoriaw@hotmail.co.uk>
2026-03-28 15:56:11 +0000

    Create buttons for rock, paper & scissors

--- foundations/javascript/rock-paper-scissors/main.js
+++ foundations/javascript/rock-paper-scissors/main.js
@@ -14,12 +14,12 @@ function getComputerChoice() {
 }
 // console.log(getComputerChoice());
 
-
+// I don't need this anymore as the human will click the button to choose their option
 // Prompting the human to type in a choice of rock, paper or scissors
-function getHumanChoice() {
-    const humanChoice = prompt("Rock, paper or scissors?");
-    return humanChoice;
-}
+// function getHumanChoice() {
+//     const humanChoice = prompt("Rock, paper or scissors?");
+//     return humanChoice;
+// }
 // console.log(getHumanChoice());
 
 
@@ -27,10 +27,7 @@ function getHumanChoice() {
 let humanScore = 0;
 let computerScore = 0;
 
-
-function playGame() {
-    // Playing a round
-    function playRound(humanChoice, computerChoice) {
+function playRound(humanChoice, computerChoice) {
         humanChoice = humanChoice.toLowerCase();
 
         if (humanChoice === computerChoice) {
@@ -51,23 +48,44 @@ function playGame() {
         console.log(`Score: Human ${humanScore} - Computer ${computerScore}`);
     }
 
+
+function playGame() {
+
     //Display the human choice and the computer choice
     const humanSelection = getHumanChoice();
     const computerSelection = getComputerChoice();
 
     //Play a round
     playRound(humanSelection, computerSelection);
-
-    
 }
 
 
-for (let i = 0; i < 5; i++) {
-    playGame();
-};
+// for (let i = 0; i < 5; i++) {
+//     playGame();
+// };
 
 // playGame();
 // playGame();
 // playGame();
 // playGame();
 // playGame();
+
+let rockButton = document.getElementById('rock');
+let paperButton = document.getElementById('paper');
+let scissorsButton = document.getElementById('scissors')
+
+rockButton.addEventListener('click', playRock);
+function playRock() {
+    playRound('rock', getComputerChoice());
+};
+
+paperButton.addEventListener('click', playPaper);
+function playPaper() {
+    playRound('paper', getComputerChoice());
+}
+
+scissorsButton.addEventListener('click', playScissors);
+function playScissors() {
+    playRound('scissors', getComputerChoice());
+}
+
