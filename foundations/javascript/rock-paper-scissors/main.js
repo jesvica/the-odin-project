68c40a5c14d5b15465233318539b8f71eab299eb
Jessica Williams <jvictoriaw@hotmail.co.uk>
2026-04-11 23:05:49 +0100

    Update styling, add images, add pop up modal to display winner

--- foundations/javascript/rock-paper-scissors/main.js
+++ foundations/javascript/rock-paper-scissors/main.js
@@ -2,11 +2,14 @@
 let displayRoundWinner = document.getElementById('round-winner')
 
 // Display the score in the DOM
-let displayScore = document.getElementById('score');
+let displayCompScore = document.getElementById('comp-score');
+let displayHumanScore = document.getElementById('human-score')
 
 // Display the overall winner in the DOM
 let displayOverallWinner = document.getElementById('overall-winner');
 
+let modal = document.getElementById('game-over-modal');
+let modalImage = document.getElementById('modal-image');
 
 // Getting the computer choice of rock, paper or scissors
 function getComputerChoice() {
@@ -39,13 +42,20 @@ function checkGameOver() {
 
         if (humanScore >= 5) {
                 displayRoundWinner.innerText = "";
-                displayOverallWinner.innerText = "Human wins!"
+                modalImage.src = "images/skeleton-lost.jpeg";
+                displayOverallWinner.innerText = "HUMAN WINS"
             } else if (computerScore >= 5) {
                 displayRoundWinner.innerText = "";
-                displayOverallWinner.innerText = "Computer wins!"
+                modalImage.src = "images/skeleton-won.jpeg";
+                displayOverallWinner.innerText = "SKELETON WINS"
             }
-    }
+        
+        modal.classList.remove('hidden');
+    } 
 }
+
+
+
 
 // Play a round of rock paper scissors
 function playRound(humanChoice, computerChoice) {
@@ -61,13 +71,14 @@ function playRound(humanChoice, computerChoice) {
             ) 
         {
             humanScore++;
-            displayRoundWinner.innerText = `You win! ${humanChoice} beats ${computerChoice}`;
+            displayRoundWinner.innerText = `Human picked ${humanChoice}. Skeleton picked ${computerChoice}.\n Human wins!`;
         } else {
             computerScore++;
-            displayRoundWinner.innerText = `You lose! ${computerChoice} beats ${humanChoice}`
+            displayRoundWinner.innerText = `Skeleton picked ${computerChoice}. Human picked ${humanChoice}.\n Skeleton wins!`
         }
         //Log the score to the console
-        displayScore.innerText = `Score: Human ${humanScore} - Computer ${computerScore}`;
+        displayHumanScore.innerText = `Human\n ${humanScore}`;
+        displayCompScore.innerText = `Skeleton\n ${computerScore}`;
 
         // Check if the game is over
         checkGameOver();
@@ -117,13 +128,17 @@ let resetScores = document.getElementById('restart')
 
 resetScores.addEventListener('click', clearScore);
 function clearScore() {
+    modal.classList.add('hidden');
+
     humanScore = 0;
     computerScore = 0;
 
     displayRoundWinner.innerText = "";
-    displayScore.innerText = "";
+    displayHumanScore.innerText = "";
+    displayCompScore.innerText = "";
     displayOverallWinner.innerText = "";
 
+    
     rockButton.disabled = false;
     paperButton.disabled = false;
     scissorsButton.disabled = false;
